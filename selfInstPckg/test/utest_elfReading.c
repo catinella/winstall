839d94e97c26e93b58828b981274a7e8502800cd
Silvano Catinella <silvano.catinella@fimer.com>
2026-07-20 23:40:57 +0200

    backup version

--- selfInstPckg/test/utest_elfReading.c
+++ selfInstPckg/test/utest_elfReading.c
@@ -19,12 +19,41 @@
 ------------------------------------------------------------------------------------------------------------------------------*/
 #include <elfReading.h>
 #include <minute.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #define TEXTFILE "/etc/passwd"
+#define MYPROC   "/proc/self/exe"
 
 TEST (get_elf_size, with_text)
 {
-	ASSERT_EQ (1, 1);
+	int   size = get_elf_size();
+	FILE  *fhS = NULL;
+	FILE  *fhT = NULL;
+	bool  err  = false;
+
+	ASSERT_TRUE (size > 0);
+
+	if ((fhT = fopen(MYPROC, "r")) == NULL) {
+		// ERROR
+		fprintf(stderr, "ERROR! I cannot open the \"%s\" file\n", MYPROC);
+
+	} else if (fseeko(fhT, size, SEEK_SET) < 0) {
+		// ERROR
+		fprintf(stderr, "ERROR! fseeco() failed\n");
+
+	} else if ((fhS = fopen(TEXTFILE, "r")) == NULL) {
+		// ERROR
+		fprintf(stderr, "ERROR! I cannot open the \"%s\" file\n", TEXTFILE);
+	
+	} else {
+		while (err == false) {
+			// TODO: read a line from MYPROC
+			// TODO: read a line from TEXTFILE
+			// TODO: data comparing...
+		}
+		ASSERT_TRUE (err == false);
+	}
 }
 
 #include "./utest_elfReading__main.sgc"
