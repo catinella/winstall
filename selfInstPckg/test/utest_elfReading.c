daa05388eb3c572d31957f5b686501bc0d78a62f
Silvano Catinella <silvano.catinella@fimer.com>
2026-07-21 16:58:35 +0200

    First completed version

--- selfInstPckg/test/utest_elfReading.c
+++ selfInstPckg/test/utest_elfReading.c
@@ -22,11 +22,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define TEXTFILE "/etc/passwd"
-#define MYPROC   "/proc/self/exe"
+#ifndef TEXTFILE
+#error "\"TEXTFILE\" is undefined symbol"
+#endif
 
-TEST (get_elf_size, with_text)
-{
+#define MYPROC     "/proc/self/exe"
+#define MAXLINELEN 1024
+
+TEST (get_elf_size, with_text) {
 	int   size = get_elf_size();
 	FILE  *fhS = NULL;
 	FILE  *fhT = NULL;
@@ -47,11 +50,29 @@ TEST (get_elf_size, with_text)
 		fprintf(stderr, "ERROR! I cannot open the \"%s\" file\n", TEXTFILE);
 	
 	} else {
-		while (err == false) {
-			// TODO: read a line from MYPROC
-			// TODO: read a line from TEXTFILE
-			// TODO: data comparing...
+		char         lineFileA[MAXLINELEN];
+		char         lineFileB[MAXLINELEN];
+		unsigned int counter = 0;
+
+		memset(lineFileA, '\0', sizeof(char) * MAXLINELEN);
+		memset(lineFileB, '\0', sizeof(char) * MAXLINELEN);
+
+		while (err == false && feof(fhS) == 0 && feof(fhT) == 0) {
+			if (fgets(lineFileA, MAXLINELEN, fhS) != NULL && fgets(lineFileB, MAXLINELEN, fhT) != NULL) {
+				if (strcmp(lineFileA, lineFileB) != 0) {
+					// ERROR! 
+					fprintf(stderr, "ERROR! Text data corrupted");
+					err = true;
+
+				} else if (fileArgumentsDb_get("verbose", NULL) == true) {
+					counter++;
+					printf("\rPassed: %d", counter);
+				}
+			}
 		}
+		if (fileArgumentsDb_get("verbose", NULL) == true)
+			printf("\n");
+
 		ASSERT_TRUE (err == false);
 	}
 }
