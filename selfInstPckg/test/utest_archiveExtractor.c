81d9c5e9b7ddd6c26b7f1584e661a6153547a82d
Silvano Catinella <silvano.catinella@fimer.com>
2026-07-27 17:22:13 +0200

    Completed version

--- selfInstPckg/test/utest_archiveExtractor.c
+++ selfInstPckg/test/utest_archiveExtractor.c
@@ -23,18 +23,65 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
-TEST (archiveExtractor, fooDataExtraction) {
+TEST (archiveExtractor, openAndClose) {
 	sexiErrorCode_t ec = SEXIEC_SUCCESS;
+	
+	// Warning: you are closing the archive but it was not open 
 	ec = archiveExtractor_close();
 	ASSERT_TRUE (SEXIEC_ISWARNING(ec));
 
+	// TGZ archive opening...
 	ec = archiveExtractor_open();
 	ASSERT_TRUE (SEXIEC_ISERROR(ec) == false);
-	
+
+	// TGZ archive closing...
 	ec = archiveExtractor_close();
 	ASSERT_TRUE (SEXIEC_ISSUCCESS(ec));
 }
 
+TEST (archiveExtractor, fooDataExtraction) {
+	struct stat     statbuf;
+	sexiErrorCode_t ec = SEXIEC_SUCCESS;
+	int             x = 0;
+
+	// TGZ archive opening...
+	ec = archiveExtractor_open();
+	ASSERT_TRUE (SEXIEC_ISERROR(ec) == false);
+
+	// File extraction
+	ec = archiveExtractor_extract("fooData/d2");
+	ASSERT_TRUE (SEXIEC_ISERROR(ec) == false);
+
+	// checking for the extracted file
+	x = stat("fooData/d2/due", &statbuf);
+      ASSERT_EQ(x , 0);
+
+	if (x == 0) {
+		char buffer[1024];
+		FILE *fh = fopen("fooData/d2/due", "r");
+		memset(buffer, '\0', sizeof(buffer));
+
+		if (fh) {
+			if (fgets(buffer, sizeof(buffer), fh) != NULL) {
+				ASSERT_EQ(strcmp("22222222", buffer), 0);
+			} else {
+				// ERROR!
+				fprintf(stderr, "TEST SKIPPED\n");
+				fprintf(stderr, "ERROR! impossible to read \"%s\" file content\n", "fooData/d2/due");
+			}
+			fclose(fh);
+		}
+	} else {
+		// ERROR!
+		fprintf(stderr, "TEST SKIPPED\n");
+		fprintf(stderr, "ERROR! I cannot open the \"%s\"\n", "fooData/d2/due");
+	}
+
+	// TGZ archive closing...
+	ec = archiveExtractor_close();
+	ASSERT_TRUE (SEXIEC_ISSUCCESS(ec));
+}
 
 #include "./utest_archiveExtractor__main.sgc"
